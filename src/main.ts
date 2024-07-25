6af5d0be4103748a29c58b04832e70716737c621
Ndro <ndro.lozano@gmail.com>
2024-07-25 18:17:25 +0200

    chore: add Prettier config file

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import cors from "cors";
-import "dotenv/config";
-import express from "express";
+import cors from 'cors'
+import 'dotenv/config'
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.get("/api", (_req, res) => {
-    res.status(200).json({ message: "Hello from the server!" });
-});
+app.get('/api', (_req, res) => {
+    res.status(200).json({ message: 'Hello from the server!' })
+})
 
 app.listen(PORT, () => {
-    console.log(`Server is running`);
-});
+    console.log(`Server is running`)
+})
