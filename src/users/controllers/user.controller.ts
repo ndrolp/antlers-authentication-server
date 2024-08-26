66243b767ab3e03e19f9eec080d569658f10bf68
Ndrolp <ndro.lozano@gmail.com>
2024-08-26 21:51:05 +0200

    feat: base get users route

--- src/users/controllers/user.controller.ts
+++ src/users/controllers/user.controller.ts
@@ -2,12 +2,17 @@ import { Request, Response } from 'express'
 import { Controller } from 'core/decorators/controller'
 import { Route } from 'core/decorators/route'
 import { Validate } from 'core/decorators/validate'
-import { User } from 'users/models/user'
+import { IUser, User } from 'users/models/user'
 import { createUserValidation } from '../validators/users'
 import { CreateUserBody } from '../dtos/users'
 import { hash } from 'argon2'
 import logging from 'core/config/logger'
 import { Authenticate } from 'auth/decorators/authenticate'
+
+export interface IUserFilter extends IUser {
+    page: number
+    limit: number
+}
 
 @Controller('/users')
 export class UserController {
@@ -27,6 +32,28 @@ export class UserController {
             return res.status(201).json(newUser)
         } catch (error) {
             logging.error(error)
+            return res.status(400).json(error)
+        }
+    }
+
+    @Route('get', '/')
+    @Authenticate()
+    async getAllUsers(
+        req: Request<object, object, object, { limit: string; page: string }>,
+        res: Response,
+    ) {
+        try {
+            const limit = parseInt(req.query.limit ?? 20)
+            const skip = (parseInt(req.query.page ?? 1) - 1) * limit
+
+            const users = await User.find()
+                .sort('username')
+                .limit(limit)
+                .skip(skip)
+            return res.json(users)
+        } catch (error) {
+            logging.error(error)
+
             return res.status(400).json(error)
         }
     }
